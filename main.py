55ea5df0ef513c1843c9f18186309668cad62f11
tem-mars <teerapatboriboon4202@gmail.com>
2024-08-24 00:56:29 +0700

    change word

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from constants import *
 def main():
     print("Starting asteroids!")
     print(f"screen width: {SCREEN_WIDTH}")
-    print(f"screen hight: {SCREEN_HEIGHT}")
+    print(f"screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
