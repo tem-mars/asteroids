257b914c2471d11c33bd1bb8f61d14e18cc72b44
tem-mars <teerapatboriboon4202@gmail.com>
2024-08-24 03:18:10 +0700

    add draw player object

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        
+        # update dt from 0 to 60 and limit the framrate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 
